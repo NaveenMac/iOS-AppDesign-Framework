9de1f812f4b9eb419c543f4265d8402dde4eb1af
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-11-01 13:13:09 +0530

    minor change

--- AppDesignFramework/Helpers/Extension.swift
+++ AppDesignFramework/Helpers/Extension.swift
@@ -169,30 +169,30 @@ extension UIFont {
 }
 
 extension UIView {
-//    static func addColors(colors: [UIColor]) {
-//        let gradientLayer = CAGradientLayer()
-//        gradientLayer.frame = self.bounds
-//
-//        var colorsArray: [CGColor] = []
-//        var locationsArray: [NSNumber] = []
-//        for (index, color) in colors.enumerated() {
-//            // append same color twice
-//            colorsArray.append(color.cgColor)
-//            colorsArray.append(color.cgColor)
-//            locationsArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index)))
-//            locationsArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index + 1)))
-//        }
-//
-//        gradientLayer.colors = colorsArray
-//        gradientLayer.locations = locationsArray
-//
-//        self.backgroundColor = .clear
-//        self.layer.addSublayer(gradientLayer)
-//
-//        // This can be done outside of this funciton
-//        //self.layer.cornerRadius = self.bounds.height / 2
-//        self.layer.masksToBounds = true
-//    }
+     func addColors(colors: [UIColor]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.bounds
+
+        var colorsArray: [CGColor] = []
+        var locationsArray: [NSNumber] = []
+        for (index, color) in colors.enumerated() {
+            // append same color twice
+            colorsArray.append(color.cgColor)
+            colorsArray.append(color.cgColor)
+            locationsArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index)))
+            locationsArray.append(NSNumber(value: (1.0 / Double(colors.count)) * Double(index + 1)))
+        }
+
+        gradientLayer.colors = colorsArray
+        gradientLayer.locations = locationsArray
+
+        self.backgroundColor = .clear
+        self.layer.addSublayer(gradientLayer)
+
+        // This can be done outside of this funciton
+        //self.layer.cornerRadius = self.bounds.height / 2
+        self.layer.masksToBounds = true
+    }
     
    static func VStack(spacing:CGFloat,alignment:UIStackView.Alignment = .top, distribution:UIStackView.Distribution)->UIStackView{
        let stack = UIStackView()
