9c0c779382e306b5c38df15034da9cb0d7415e51
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-11-25 14:58:51 +0530

    alert updated

---
+++ AppDesignFramework/Helpers/Extensions/Extension_UIAlert.swift
@@ -0,0 +1,21 @@
+//
+//  Extension_UIAlert.swift
+//  FileManager
+//
+//  Created by Naveen Chauhan on 25/11/21.
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    static func showError(title:String,message:String)-> UIAlertController{
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default , handler:{ (UIAlertAction)in
+           
+        }))
+       return alert
+    }
+   
+}
