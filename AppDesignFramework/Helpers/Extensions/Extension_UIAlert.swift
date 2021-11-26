3afa9792c1c9205b28b04526de90c4b3faf8aa3f
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-11-26 15:00:30 +0530

    alert title color change class added

--- AppDesignFramework/Helpers/Extensions/Extension_UIAlert.swift
+++ AppDesignFramework/Helpers/Extensions/Extension_UIAlert.swift
@@ -19,3 +19,15 @@ extension UIAlertController {
     }
    
 }
+
+extension UIAlertAction {
+    var titleTextColor: UIColor? {
+        get {
+            return self.value(forKey: "titleTextColor") as? UIColor
+        } set {
+            self.setValue(newValue, forKey: "titleTextColor")
+        }
+    }
+    
+   
+}
