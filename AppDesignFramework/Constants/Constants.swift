5e6e2f982fe9a0d8a6f6fa4f07c2ac190371b6a9
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-12-16 07:56:42 +0530

    textfield done toolbar added

--- AppDesignFramework/Constants/Constants.swift
+++ AppDesignFramework/Constants/Constants.swift
@@ -13,7 +13,7 @@ struct Constants {
 
     static let USER_DIR = "user"
 
-    static let JWT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGlnaWxvY2tlci5nb3YuaW4iLCJhdWQiOiJESUdJTE9DS0VSIiwiaWF0IjoxNjM5NjEzMDExLCJuYmYiOjE2Mzk2MTI5NTEsImV4cCI6MTYzOTYxNjYxMSwiZGF0YSI6eyJzdGF0dXMiOiJzdWNjZXNzIiwidXNlcm5hbWUiOiJOeFY4Y21vNzNHdUxtTHVldUlTTVlGdFRPVmVQbE1BZWZGS0hRcGRKR3FXeU9seEx3NU1id3BCYkdRVFlNYnZTIiwiZGlnaWxvY2tlcmlkIjoiMTgyZjdkMjAtYzVjZS01OTIyLWJhZjMtOWU5ZmYxYWYwYjVlIn19.uZa4KAUzAj-KwWw0EhZw0ABPcuxZKGtmJEor9caqKwo"
+    static let JWT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGlnaWxvY2tlci5nb3YuaW4iLCJhdWQiOiJESUdJTE9DS0VSIiwiaWF0IjoxNjM5NjE5OTQ3LCJuYmYiOjE2Mzk2MTk4ODcsImV4cCI6MTYzOTYyMzU0NywiZGF0YSI6eyJzdGF0dXMiOiJzdWNjZXNzIiwidXNlcm5hbWUiOiJOeFY4Y21vNzNHdUxtTHVldUlTTVlGdFRPVmVQbE1BZWZGS0hRcGRKR3FXeU9seEx3NU1id3BCYkdRVFlNYnZTIiwiZGlnaWxvY2tlcmlkIjoiMTgyZjdkMjAtYzVjZS01OTIyLWJhZjMtOWU5ZmYxYWYwYjVlIn19.NbpiCQnwoqD8VfUoB7nscL02bpCUhyIUjyV1LHXXrqA"
 
     static let DEFAULT_FILES = """
                              {"status":"success","data":[{"Name":"Folder 1","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"Folder 2","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"Folder 3","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"File 1","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"},{"Name":"File 2","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"},{"Name":"File 3","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"}]}
--- AppDesignFramework/Helpers/Extensions/Extension_UITextField.swift
+++ AppDesignFramework/Helpers/Extensions/Extension_UITextField.swift
@@ -73,15 +73,38 @@ extension UITextField {
 
             let toolbar: UIToolbar = UIToolbar()
             toolbar.barStyle = .default
-            toolbar.items = [
-                UIBarButtonItem(title: "Cancel", style: .plain, target: onCancel.target, action: onCancel.action),
-                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
-                UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action)
-            ]
+        toolbar.items = [
+            UIBarButtonItem(title: "Cancel", style: .plain, target: onCancel.target, action: onCancel.action),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            
+            UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action)
+        ]
+            
             toolbar.sizeToFit()
 
             self.inputAccessoryView = toolbar
         }
+    
+    func addNextCancelToolbar(onNext: (target: Any, action: Selector)? = nil, onCancel: (target: Any, action: Selector)? = nil) {
+            let onCancel = onCancel ?? (target: self, action: #selector(cancelButtonTapped))
+            let onNext = onNext ?? (target: self, action: #selector(doneButtonTapped))
+
+            let toolbar: UIToolbar = UIToolbar()
+            toolbar.barStyle = .default
+        let next = UIBarButtonItem(title: "Next", style: .done, target: onNext.target, action: onNext.action)
+        next.tag = self.tag
+        toolbar.items = [
+            UIBarButtonItem(title: "Cancel", style: .plain, target: onCancel.target, action: onCancel.action),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            next
+            
+        ]
+            
+            toolbar.sizeToFit()
+
+            self.inputAccessoryView = toolbar
+        }
+    
     @objc func doneButtonTapped() { self.resignFirstResponder() }
     @objc func cancelButtonTapped() { self.resignFirstResponder() }
     
