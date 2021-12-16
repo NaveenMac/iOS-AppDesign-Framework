31a9fd8a07a6b1454fa6bfd883c3b08c404901d1
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-12-16 06:08:36 +0530

    back button set

--- AppDesignFramework/Constants/Constants.swift
+++ AppDesignFramework/Constants/Constants.swift
@@ -13,7 +13,7 @@ struct Constants {
 
     static let USER_DIR = "user"
 
-    static let JWT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGlnaWxvY2tlci5nb3YuaW4iLCJhdWQiOiJESUdJTE9DS0VSIiwiaWF0IjoxNjM5NTcwNzM1LCJuYmYiOjE2Mzk1NzA2NzUsImV4cCI6MTYzOTU3NDMzNSwiZGF0YSI6eyJzdGF0dXMiOiJzdWNjZXNzIiwidXNlcm5hbWUiOiJOeFY4Y21vNzNHdUxtTHVldUlTTVlGdFRPVmVQbE1BZWZGS0hRcGRKR3FXeU9seEx3NU1id3BCYkdRVFlNYnZTIiwiZGlnaWxvY2tlcmlkIjoiMTgyZjdkMjAtYzVjZS01OTIyLWJhZjMtOWU5ZmYxYWYwYjVlIn19.TyhO7k3yRfF-MYa4oSNrLWAOGUSmN3aX9ZUDe3I-y6E"
+    static let JWT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGlnaWxvY2tlci5nb3YuaW4iLCJhdWQiOiJESUdJTE9DS0VSIiwiaWF0IjoxNjM5NjEzMDExLCJuYmYiOjE2Mzk2MTI5NTEsImV4cCI6MTYzOTYxNjYxMSwiZGF0YSI6eyJzdGF0dXMiOiJzdWNjZXNzIiwidXNlcm5hbWUiOiJOeFY4Y21vNzNHdUxtTHVldUlTTVlGdFRPVmVQbE1BZWZGS0hRcGRKR3FXeU9seEx3NU1id3BCYkdRVFlNYnZTIiwiZGlnaWxvY2tlcmlkIjoiMTgyZjdkMjAtYzVjZS01OTIyLWJhZjMtOWU5ZmYxYWYwYjVlIn19.uZa4KAUzAj-KwWw0EhZw0ABPcuxZKGtmJEor9caqKwo"
 
     static let DEFAULT_FILES = """
                              {"status":"success","data":[{"Name":"Folder 1","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"Folder 2","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"Folder 3","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"Collection"},{"Name":"File 1","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"},{"Name":"File 2","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"},{"Name":"File 3","Key":"hello","human_size":"1 MB","LastModified":"10-10-2010","Size":"2MB","Ext":"PDF"}]}
