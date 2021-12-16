c536119d4a654ab96a08ef3ef27f4abb9ef41d37
naveen.chauhan <naveen.chauhan@digitalindia.gov.in>
2021-12-16 14:08:08 +0530

    table view issues fixed

--- AppDesignFramework/Helpers/View-Components/SelfSizedTableView.swift
+++ AppDesignFramework/Helpers/View-Components/SelfSizedTableView.swift
@@ -9,17 +9,56 @@ import UIKit
 
 class SelfSizedTableView: UITableView {
     var maxHeight: CGFloat = UIScreen.main.bounds.size.height
-    
+    var showShadow:Bool = false
     override func reloadData() {
         super.reloadData()
         self.invalidateIntrinsicContentSize()
         self.layoutIfNeeded()
+        if showShadow {
+            if self.contentSize.height > 1 || self.contentSize.width > 1 {
+//                self.layer.shadowColor = UIColor(hex: "#A7A7A7FF")?.cgColor ?? UIColor.black.cgColor
+//                self.layer.shadowOpacity = 1
+//                self.layer.shadowOffset = .zero
+//                self.layer.shadowRadius = 10
+//                self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+                self.dropTableShadow(color: UIColor(hex: "#A7A7A7FF") ?? .clear, opacity: 0.1, offSet:CGSize(width:1,height:1), radius: 10.0, scale: true)
+            }
+           
+        }
     }
     
     override var intrinsicContentSize: CGSize {
         setNeedsLayout()
         layoutIfNeeded()
         let height = min(contentSize.height, maxHeight)
+    
         return CGSize(width: contentSize.width, height: height)
     }
+    
+    // OUTPUT 1
+    func dropTableShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 1
+
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+      }
+
+      // OUTPUT 2
+      func dropTableShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+
+        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+      }
+    
 }
